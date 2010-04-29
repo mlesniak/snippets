9c06ff651eff2d3a8d6a118032a0baf76dd1a1a9
Michael Lesniak <mlesniak@platon.(none)>
2010-04-29 19:21:19 +0200

    PPM support added.
    - Image is now stored into a file.
    - PPM type added.
    - Removed console output.

--- Mandelbrot.hs
+++ Mandelbrot.hs
@@ -1,7 +1,22 @@
 -- Mandelbrotset in Haskell.
+--
+-- TODO
+--   * error message for command line parameters
 module Main where
 import System.Environment
+import Text.Printf
 import Data.List
+
+
+-- A PPM file describes the filename and dimensions of an image. Since it is
+-- monochrome, it stores the values of the pixels as booleans.
+data PPM = PPMFile {
+      ppmName   :: String
+    , ppmWidth  :: Int
+    , ppmHeight :: Int
+    , ppmData   :: [Bool] -- Should be abstracted later to a generalized
+                          -- image type.
+} 
 
 main :: IO ()
 main = do
@@ -10,24 +25,25 @@ main = do
     let width   = read width'  :: Double
         height  = read height' :: Double
 
-        -- Map coordinates of the image to the Mandelbrot set.
-        points  = [(x,y)| y <- [0..height-1], x <- [0..width-1]]
+    -- Map coordinates of the image to the Mandelbrot set.
+    let points  = [(x,y)| y <- [0..height-1], x <- [0..width-1]]
         diffX   = 3.0 / (width-1)
         diffY   = 2.0 / (height-1)
         mpoints = map (\(x,y) -> (-2 + x*diffX, -1 + y*diffY)) points
         values  = map (f 100) mpoints
-    printSet (fromEnum width+1) $ values
+        ppm     = PPMFile filename (fromEnum width) (fromEnum height) values
+
+    writePPMFile ppm
 
 
--- Print set on the console
-printSet :: Int -> [Bool] -> IO ()
-printSet w ps = mapM_ (putStrLn . map boolToChar) $ splitLines ps
-  where boolToChar True  = '*'
-        boolToChar False = ' '
-        splitLines [] = []
-        splitLines ps = 
-            let (line, rest) = splitAt (w-1) ps
-            in line : splitLines rest
+-- Writes a PPMFile.
+writePPMFile :: PPM -> IO ()
+writePPMFile (PPMFile name w h img) = do
+   let header = printf "P1\n%d %d\n1\n" w h
+       imgd   = intercalate " " $ map boolToNum img
+   writeFile name (header ++ imgd)
+ where boolToNum True  = "1"
+       boolToNum False = "0"
 
 
 -- Return true, if the point (x,y) is in the set. Tests for iter iterations.
