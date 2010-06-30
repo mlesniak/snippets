db9d54316a91711cef51b37780329d5b31e66267
Michael Lesniak <michael.lesniak@gmail.com>
2010-06-30 14:34:34 +0200

    Animated version of the small cairo example.

--- GTKCairo.hs
+++ GTKCairo.hs
@@ -2,6 +2,8 @@ module Main where
 import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Glade
+import Data.IORef
+import Control.Concurrent
 
 
 main = do
@@ -10,21 +12,38 @@ main = do
     window   <- xmlGetWidget xml castToWindow "window"
     close    <- xmlGetWidget xml castToButton "button"
 
+
+    -- Mutable value for animation
+    ref <- newIORef 0.0
+
     darea    <- xmlGetWidget xml castToDrawingArea "drawingArea"
     darea `on` sizeRequest $ return (Requisition 400 400)
-    (darea `on` exposeEvent) update
+    (darea `on` exposeEvent) (update ref)
+    
+    -- Fork thread to change value
+    forkIO (thread ref darea)
 
     onClicked close (widgetDestroy window)
-
     onDestroy window mainQuit
     widgetShowAll window
     mainGUI
 
 
-update :: EventM EExpose Bool
-update = do
+thread :: IORef Double -> DrawingArea -> IO ()
+thread ref dr = do
+    putStrLn "tick"
+    threadDelay (10^5)
+    modifyIORef ref (\rv -> toEnum $ (fromEnum rv + 5) `mod` 400)
+    -- Repaint the widget with the new coordinate.
+    widgetQueueDraw dr
+    thread ref dr
+
+
+update :: IORef Double -> EventM EExpose Bool
+update ref = do
     win   <- eventWindow
     liftIO $ do
+    xpos <- readIORef ref
     (w,h) <- drawableGetSize win
     let width  = realToFrac w
         height = realToFrac h
@@ -36,7 +55,7 @@ update = do
 
         setSourceRGB 0 0 0
 
-        moveTo 30 30
+        moveTo xpos 30
         lineTo (width-30) (height-30)
         stroke
 
